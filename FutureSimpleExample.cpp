b1c76f8e2f2389e14ca21ca5c273facd6c6e551c
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2017-12-02 20:15:57 +0530

    Add files via upload

---
+++ FutureSimpleExample.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <future>
+#include <thread>
+#include <vector>
+#include <string>
+
+int getMax(std::vector<int> inputVec)
+{
+    std::cout<<"in getMax \n";
+
+    return *(std::max_element(inputVec.begin(),inputVec.end()));
+}
+int main()
+{
+    std::vector<int> nums = {761,52,33,14,56,78,90};
+    std::future<int> f = std::async(std::launch::async,getMax,nums);
+
+    std::cout<< "getting the result in Main"<<std::endl;
+
+    // std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout<< f.get();
+}
