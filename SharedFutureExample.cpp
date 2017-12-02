d2a8481c2d504d0782c85df79e1dd4c3dae6c809
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2017-12-02 22:03:06 +0530

    Add files via upload

---
+++ SharedFutureExample.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <future>
+#include <thread>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::promise<void> p2;
+    std::shared_future<void> sf = p2.get_future();
+    std::mutex m;
+
+    std::future<void> f1 = std::async(std::launch::async, [&sf, &m]()
+                                      {sf.wait();
+                                          m.lock();std::cout<<"received signal from promise in thread1 \n";m.unlock();}
+    );
+
+    std::future<void> f2 = std::async(std::launch::async,[&sf, &m]()
+                                      {sf.wait();
+                                          m.lock();std::cout<<"received signal from promise in thread2 \n";m.unlock();}
+    );
+
+    p2.set_value();
+
+    f1.wait();
+    f2.wait();
+}
