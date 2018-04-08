0d16fe695f43d86fd0a08f816fcb4cf035152dd0
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2018-04-08 18:03:56 +0530

    cosmetic change

--- FuturePromiseExample.cpp
+++ FuturePromiseExample.cpp
@@ -6,7 +6,10 @@ int main()
 {
 	std::promise<int> p;
 	std::future<int> f3 = p.get_future();
-	std::thread do_work([&p]() {std::cout << "working \n"; p.set_value(5); });
+	std::thread do_work([&p]() 
+			    {std::cout << "working \n"; 
+			     p.set_value(5); }
+			   );
 	do_work.detach();
 	std::cout << f3.get() << std::endl;
 }
