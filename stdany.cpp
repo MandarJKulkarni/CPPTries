d170206dd1d384726a17326deb1f141db4fc4d4c
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2018-04-09 12:20:43 +0530

    Fixed errors

--- stdany.cpp
+++ stdany.cpp
@@ -1,34 +1,30 @@
-// stdanyvariantoptional.cpp : Defines the entry point for the console application.
-//
-
-#include <any>
 #include <iostream>
+#include <any>
+#include <vector>
 int main()
 {
-	auto i = std::make_any<int>(20);
-	if(i.has_value())
-		std::cout << std::any_cast<int> (i) << std::endl;
-	try
-	{
-		std::string str = std::any_cast<std::string> (i);
-	}
-	catch (const std::bad_any_cast &ex)
-	{
-		std::cout << "Caught bad_any_cast exception " << ex.what() << "\n";
-	}
-	std::cout << i.type().name() << "\n";
+    auto i = std::make_any<int>(20);
+    if(i.has_value())
+        std::cout << std::any_cast<int> (i) << std::endl;
+    try
+    {
+        std::string str = std::any_cast<std::string> (i);
+    }
+    catch (const std::bad_any_cast &ex)
+    {
+        std::cout << "Caught bad_any_cast exception " << ex.what() << "\n";
+    }
+    std::cout << i.type().name() << "\n";
 
-	//For an empty any object, returned type info is typeid(void)
-	std::any emptyObj;
-	std::cout << emptyObj.type().name() << "\n";
+    //For an empty any object, returned type info is typeid(void)
+    std::any emptyObj;
+    std::cout << emptyObj.type().name() << "\n";
 
-	//Creating a vector of different data types
-	auto a = std::any(22);
-	auto f = std::make_any<float>(9.9);
-	auto str = std::make_any<std::string> ("str");
-	std::vector<std::any> v {a,f,str};
-	std::cout << std::any_cast<int>(v[0]) << '\n';
-	
-	return 0;
+    auto a = std::any(22);
+    auto f = std::make_any<float>(9.9);
+    auto str = std::make_any<std::string> ("str");
+
+    std::vector<std::any> v {a,f,str};
+    std::cout << std::any_cast<int>(v[0]) << '\n';
+    return 0;
 }
-
