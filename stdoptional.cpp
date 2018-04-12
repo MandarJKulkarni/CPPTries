965442f158cfa199a9bb66a1ccd9b7707eca140a
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2018-04-12 19:34:14 +0530

    Sample program for std::optional

---
+++ stdoptional.cpp
@@ -0,0 +1,18 @@
+
+#include <optional>
+#include <iostream>
+
+int main()
+{
+	std::optional<int> opt1 = std::nullopt;
+	std::optional<std::string> optStr = std::make_optional<std::string>("str");
+	std::optional<int> opt = 2;
+	if (opt.has_value())
+	{
+		int val = *opt;
+		int val2 = opt.value();
+	}
+
+	return opt.value();
+}
+
