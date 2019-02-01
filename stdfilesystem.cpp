6056ddaba179899acf5eda08436fb7ce75be7435
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2019-02-01 22:43:54 +0530

    create and delete a temporary folder

--- stdfilesystem.cpp
+++ stdfilesystem.cpp
@@ -1,8 +1,20 @@
 #include <filesystem>
-
+#include <iostream>
 int main()
 {
-	//returns true for existing folder
-	bool exists = std::filesystem::exists("D:\\existingFolder");
+	bool exists = std::filesystem::exists("D:\\NotBackedUp");
+	//returns size in bytes
+	const auto fileSize{ std::filesystem::file_size("D:\\NotBackedUp\\MyFiles\\MyPhoto.pdf") };
+	//returns empty space on D drive in bytes
+	const auto availableSpace = std::filesystem::space("D:\\NotBackedUp\\MyFiles").available;
+	
+	std::filesystem::path tempFolder("D:\\NotBackedUp\\TempFoder");
+	std::filesystem::create_directory(tempFolder);
+	exists = std::filesystem::exists(tempFolder);
+	if (exists)
+		std::filesystem::remove(tempFolder);
+	exists = std::filesystem::exists(tempFolder);
+	if (exists == false)
+		std::cout << "Created and removed a temparary folder";
 	return 0;
 }
